ea160bb13652592f294d8011813d71d272b0ffef
Erick Leandro <j.erickleandro@gmail.com>
2025-07-22 17:16:44 +0000

    fix  style linting

--- tests/orchestrator.js
+++ tests/orchestrator.js
@@ -1,11 +1,11 @@
-import retry from 'async-retry';
+import retry from "async-retry";
 async function waitForAllServices() {
   await waitForWebServer();
 
   async function waitForWebServer() {
     return retry(fetchStatusPage, {
       retries: 100,
-    })
+    });
     async function fetchStatusPage() {
       const response = await fetch("http://localhost:3000/api/v1/status");
       const responseBody = await response.json();
@@ -14,5 +14,5 @@ async function waitForAllServices() {
 }
 
 export default {
-  waitForAllServices
+  waitForAllServices,
 };
